bee0045c5c80dfe5293672233235a20cb79d1e7f
Kinwaa <kinwaa@wo.cn>
2021-12-07 23:36:39 +0800

    增加快速处理Collection to Map的方法

--- src/main/java/cc/waa/java/utils/lang/CollectionUtils.java
+++ src/main/java/cc/waa/java/utils/lang/CollectionUtils.java
@@ -6,9 +6,14 @@
  */
 package cc.waa.java.utils.lang;
 
+import static org.apache.commons.beanutils.PropertyUtils.getProperty;
+
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author  Kinwaa
@@ -16,7 +21,8 @@ import java.util.Map;
  * @version 0.0.1
  * @since   0.0.1
  */
-public final class CollectionUtils {
+@Slf4j
+ public final class CollectionUtils {
 
    /**
     * 快速创建一个Map对象.
@@ -34,6 +40,69 @@ public final class CollectionUtils {
       }
 
       return result;
+   }
+
+   /**
+    * @param <K>
+    * @param <V>
+    * @param col
+    * @param keyName
+    * @param keyType
+    * @return 保存原集合对象的Map
+    */
+   public static <K, V> Map<K, V> toMap(final Collection<V> col,
+                                        final String keyName,
+                                        final Class<K> keyType) {
+      if (col == null) {
+         return null;
+      }
+
+      Map<K, V> result = new HashMap<K, V>(col.size());
+
+      try {
+         for (V value : col) {
+            K key = keyType.cast(getProperty(value, keyName));
+
+            result.put(key, value);
+         }
+      } catch (ReflectiveOperationException e) {
+         log.error("pick key fail", e);
+
+         return null;
+      }
+
+      return result;
+   }
+
+   /**
+    * @param <I>
+    * @param <C>
+    * @param col
+    * @param propertyName
+    * @param itemType
+    * @param colType
+    * @return 提取的属性的集合
+    */
+   public static <I, C extends Collection<I>> C
+   pickProperty(final Collection<?> col, final String propertyName,
+                final Class<I> itemType, final Class<C> colType) {
+      if (col == null) {
+         return null;
+      }
+
+      try {
+         C result = colType.getDeclaredConstructor().newInstance();
+
+         for (Object obj : col) {
+            result.add(itemType.cast(getProperty(obj, propertyName)));
+         }
+
+         return result;
+      } catch (ReflectiveOperationException e) {
+         log.error("pick property fail", e);
+
+         return null;
+      }
    }
 
 
